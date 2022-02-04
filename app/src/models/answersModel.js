466b7e715a6ac1dd8df49411e1e95fc55c07df70
Owen Evans <owen@3sidedcube.com>
2022-02-04 10:06:59 +0000

    app folder linted and passes

--- app/src/models/answersModel.js
+++ app/src/models/answersModel.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const { ObjectId } = Schema;
 
 const AnswerResponse = new Schema({
-    name: { type: String, required: true, trim: true },
-    value: { type: String, required: false, trim: true }
+  name: { type: String, required: true, trim: true },
+  value: { type: String, required: false, trim: true }
 });
 
 const Answer = new Schema({
-    report: { type: ObjectId, required: true },
-    reportName: { type: String, required: true },
-    username: { type: String, required: false, trim: true },
-    organization: { type: String, required: false, trim: true },
-    areaOfInterest: { type: ObjectId, required: false },
-    areaOfInterestName: { type: String, required: false, trim: true },
-    language: { type: String, required: true, trim: true },
-    userPosition: { type: Array, required: false, default: [] },
-    clickedPosition: { type: Array, required: false, default: [] },
-    startDate: { type: String, required: false, trim: true },
-    endDate: { type: String, required: false, trim: true },
-    layer: { type: String, required: false, trim: true },
-    user: { type: ObjectId, required: true },
-    responses: [AnswerResponse],
-    createdAt: { type: Date, required: true, default: Date.now }
+  report: { type: ObjectId, required: true },
+  reportName: { type: String, required: true },
+  username: { type: String, required: false, trim: true },
+  organization: { type: String, required: false, trim: true },
+  areaOfInterest: { type: ObjectId, required: false },
+  areaOfInterestName: { type: String, required: false, trim: true },
+  language: { type: String, required: true, trim: true },
+  userPosition: { type: Array, required: false, default: [] },
+  clickedPosition: { type: Array, required: false, default: [] },
+  startDate: { type: String, required: false, trim: true },
+  endDate: { type: String, required: false, trim: true },
+  layer: { type: String, required: false, trim: true },
+  user: { type: ObjectId, required: true },
+  responses: [AnswerResponse],
+  createdAt: { type: Date, required: true, default: Date.now }
 });
 
-module.exports = mongoose.model('Answer', Answer);
+module.exports = mongoose.model("Answer", Answer);
--- app/src/serializers/reportsSerializer.js
+++ app/src/serializers/reportsSerializer.js
@@ -1,40 +1,45 @@
-const logger = require('logger');
-const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+const logger = require("logger");
+const JSONAPISerializer = require("jsonapi-serializer").Serializer;
 
 function createSerializer(languages) {
-    return new JSONAPISerializer('reports', {
-        attributes: [
-            'name', 'languages', 'defaultLanguage', 'user', 'answersCount', 'questions', 'createdAt', 'public', 'status'
-        ],
-        questions: {
-            attributes: ['type', 'label', 'defaultValue', 'values', 'required']
-        },
-        name: {
-            attributes: languages
-        },
-        typeForAttribute(attribute) {
-            return attribute;
-        },
-        keyForAttribute: 'camelCase'
-    });
+  return new JSONAPISerializer("reports", {
+    attributes: [
+      "name",
+      "languages",
+      "defaultLanguage",
+      "user",
+      "answersCount",
+      "questions",
+      "createdAt",
+      "public",
+      "status"
+    ],
+    questions: {
+      attributes: ["type", "label", "defaultValue", "values", "required"]
+    },
+    name: {
+      attributes: languages
+    },
+    typeForAttribute(attribute) {
+      return attribute;
+    },
+    keyForAttribute: "camelCase"
+  });
 }
 
 class ReportsSerializer {
-
-    static serialize(data) {
-        logger.debug(data);
-        if (Array.isArray(data)) {
-            const reports = { data: [] };
-            data.forEach((report) => {
-                const serializedData = createSerializer(report.languages).serialize(report);
-                reports.data.push(serializedData.data);
-            });
-            return reports;
-        }
-        return createSerializer(data.languages).serialize(data);
-
+  static serialize(data) {
+    logger.debug(data);
+    if (Array.isArray(data)) {
+      const reports = { data: [] };
+      data.forEach(report => {
+        const serializedData = createSerializer(report.languages).serialize(report);
+        reports.data.push(serializedData.data);
+      });
+      return reports;
     }
-
+    return createSerializer(data.languages).serialize(data);
+  }
 }
 
 module.exports = ReportsSerializer;
